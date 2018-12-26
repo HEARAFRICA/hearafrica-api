ad8429133f32c31f0daa10e5887487ef24608c91
romalice1 <rommy_91@hotmail.fr>
2018-12-26 12:50:58 +0200

    Create routes

--- index.js
+++ index.js
@@ -8,11 +8,29 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+/* HELPERS */
+function responserBuilder(code,message, body){
+    return { code:code,message:message,body:body }
+}
+
 /* ROUTES */
 // Index
 app.get('/', function(req, res){
     res.send("Welcome to HearAfrica")
 })
+
+//Create user
+app.post('/api/users/signup', function(req, res){
+    console.log(req.body)
+    res.json( responserBuilder(200, 'User created successfully', {}) )
+})
+
+// List all users
+app.get('/api/users', function(req, res){
+    res.json( responserBuilder(200, 'Successful', {}) )
+})
+
+// Authenticate
 
 // Start server
 var port = 4000
