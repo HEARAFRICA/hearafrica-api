aa9429c30f0e321c8b843107d55d310b800a231c
romalice1 <rommy_91@hotmail.fr>
2018-12-27 16:45:30 +0200

    Fix reported test fails

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var mongojs = require('mongojs');
 var app = express();
 
 // Initialize database 
-var db = mongojs('hearafrica',['users','sites'])
+var db = mongojs('hearafrica',['users','sites']);
 
 /** MIDDLEWARE */
 // Body parser
@@ -15,14 +15,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 /* HELPERS */
 function responserBuilder(code,message, body){
-    return { code:code,message:message,body:body }
+    return { code:code,message:message,body:body };
 }
 
 /* ROUTES */
 // Index
 app.get('/', function(req, res){
-    res.send("Welcome to HearAfrica")
-})
+    res.send("Welcome to HearAfrica");
+});
 
 //Create user
 app.post('/api/users/signup', function(req, res){
@@ -38,26 +38,26 @@ app.post('/api/users/signup', function(req, res){
         signed_in : false,
         active : true
 
-    }
+    };
     db.users.insert(newUser, function(err, results){
         if(err){
-            res.json( responserBuilder(201, err, {}) )
+            res.json( responserBuilder(201, err, {}) );
         }else{
-            res.json( responserBuilder(200, 'User created successfully', results) )
+            res.json( responserBuilder(200, 'User created successfully', results) );
         }
-    })
-})
+    });
+});
 
 // List all users
 app.get('/api/users', function(req, res){
     db.users.find(function(err, results){
         if(err){
-            res.json( responserBuilder(201, 'Failed', results) )
+            res.json( responserBuilder(201, 'Failed', results) );
         }else{
-            res.json( responserBuilder(200, 'Successful', results) )
+            res.json( responserBuilder(200, 'Successful', results) );
         }
-    })
-})
+    });
+});
 
 // Log in
 app.post('/api/users/authenticate', function(req,res){
@@ -68,15 +68,15 @@ app.post('/api/users/authenticate', function(req,res){
         new: true
     }, function(err,results){
         if(err){
-            res.json( responserBuilder(501, 'Internar Server Error', err) )
+            res.json( responserBuilder(501, 'Internar Server Error', err) );
         }
         if(results.length==0){
-            res.json( responserBuilder(201, 'Invalid username or password', results) )
+            res.json( responserBuilder(201, 'Invalid username or password', results) );
         }else{
-            res.json( responserBuilder(200, 'Authenticated', results) )
+            res.json( responserBuilder(200, 'Authenticated', results) );
         }
-    })
-})
+    });
+});
 
 // Logout
 app.get('/api/users/logout/:user_id', function(req,res){
@@ -86,11 +86,11 @@ app.get('/api/users/logout/:user_id', function(req,res){
         new: true
     }, function(err,results){
         if(err){
-            res.json( responserBuilder(501, 'Internar Server Error', err) )
+            res.json( responserBuilder(501, 'Internar Server Error', err) );
         }
-        res.json( responserBuilder(200, 'User logged out successfully', {}) )
-    })
-})
+        res.json( responserBuilder(200, 'User logged out successfully', {}) );
+    });
+});
 
 // Create a site
 app.post('/api/sites/create', function(req, res){
@@ -103,38 +103,38 @@ app.post('/api/sites/create', function(req, res){
         latitude: req.body.latitude,
         ratings: 0,
         views: 0
-    }
+    };
     db.sites.insert(newSite, function(err,results){
         if(err){
-            res.json( responserBuilder(501,'Internal Server Error' , err) )
+            res.json( responserBuilder(501,'Internal Server Error' , err) );
         }else{
-            res.json( responserBuilder(200, 'Site added successfully', results) )
+            res.json( responserBuilder(200, 'Site added successfully', results) );
         }
-    })
-})
+    });
+});
 
 // Get sites list
 app.get('/api/sites', function(req, res){
     db.sites.find(function(err, results){
         if(err){
-            res.json( responserBuilder(501,'Internal Server Error' , err) )
+            res.json( responserBuilder(501,'Internal Server Error' , err) );
         }
-        res.json( responserBuilder(200,'Success' , results) )
-    })
-})
+        res.json( responserBuilder(200,'Success' , results) );
+    });
+});
 
 // Get site by id
 app.get('/api/sites/:site_id', function(req, res){
     db.sites.findOne({_id: mongojs.ObjectId(req.params.site_id)},function(err, results){
         if(err){
-            res.json( responserBuilder(501,'Internal Server Error' , err) )
+            res.json( responserBuilder(501,'Internal Server Error' , err) );
         }
-        res.json( responserBuilder(200,'Success' , results) )
-    })
-})
+        res.json( responserBuilder(200,'Success' , results) );
+    });
+});
 
 /** Start server **/
-var port = 4000
+var port = 4000;
 app.listen(port, function(){
-    console.log("Server listening at "+port+"...")
-})
+    console.log("Server listening at "+port+"...");
+});
